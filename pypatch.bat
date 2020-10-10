be502b4563151aaf9ca21a20c4991bd558d7447a
leichau <leichau@foxmail.com>
2020-10-10 16:28:50 +0800

    update eric

--- pypatch.bat
+++ pypatch.bat
@@ -1,7 +1,7 @@
-rem if exist .\dist\SerialPort\list.ini copy .\dist\SerialPort\list.ini .\dist\list.ini
-rd /s /q C:\Users\Administrator\Desktop\PyQt5\SerialPort\build\SerialPort
-rem rd /s /q C:\Users\Administrator\Desktop\PyQt5\SerialPort\dist\SerialPort
-pyinstaller -F -w SerialPort.py
-xcopy "D:\Program Files\Python\Python36\Lib\site-packages\PyQt5\Qt\plugins\styles" .\dist\styles\
-rem if exist .\dist\list.ini copy .\dist\list.ini .\dist\SerialPort\list.ini
+@rem if exist .\dist\SerialPort\list.ini copy .\dist\SerialPort\list.ini .\dist\list.ini
+rd /s /q E:\workbench\PyQt\GSP\build\SerialPort
+@rem -F: generate single file
+@rem -w: no console for windows system
+pyinstaller -F -w Codec.py
+@rem xcopy "D:\Program Files\Python\Python36\Lib\site-packages\PyQt5\Qt\plugins\styles" .\dist\styles\
 pause
